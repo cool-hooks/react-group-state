cf27161a77469069c81226c08cce1148ab1c68dc
Jakub Biesiada <biesiadajakub@icloud.com>
2020-12-25 22:44:43 +0100

    :bookmark:  v1.0.0 beta 2

--- src/useGroupState.ts
+++ src/useGroupState.ts
@@ -1,31 +1,28 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 
 export function useGroupState<T extends object>(group: T) {
   const [state, setState] = useState(group);
 
-  const updateState = useCallback(
-    (
-      data: Partial<T> | ((state: T) => Partial<T>),
-      callback?: (state: T) => void
-    ) => {
-      const updatedState = state;
+  const updateState = (
+    data: Partial<T> | ((state: T) => Partial<T>),
+    callback?: (state: T) => void
+  ) => {
+    const updatedState = state;
 
-      const mergeState = (data: Partial<T>) => {
-        (Object.keys(data) as Array<keyof T>).map((key) => {
-          if (Object.prototype.hasOwnProperty.call(updatedState, key)) {
-            updatedState[key] = data[key] as T[keyof T];
-          }
-        });
+    const mergeState = (data: Partial<T>) => {
+      (Object.keys(data) as Array<keyof T>).map((key) => {
+        if (Object.prototype.hasOwnProperty.call(updatedState, key)) {
+          updatedState[key] = data[key] as T[keyof T];
+        }
+      });
 
-        setState(updatedState);
+      setState(updatedState);
 
-        callback?.(updatedState);
-      };
+      callback?.(updatedState);
+    };
 
-      mergeState(typeof data === 'function' ? data(state) : data);
-    },
-    [state]
-  );
+    mergeState(typeof data === 'function' ? data(state) : data);
+  };
 
   return [state, updateState] as const;
 }
